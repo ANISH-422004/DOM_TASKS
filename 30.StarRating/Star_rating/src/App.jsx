f586158eaefbd0004bd1f70a9d0661374bd8e653
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-01-03 22:54:14 +0530

    StarRatingDone

--- 30.StarRating/Star_rating/src/App.jsx
+++ 30.StarRating/Star_rating/src/App.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  let arr = new Array(10).fill(0); // we can get this form user
 
-export default App
+  const [rating, setRating] = useState(0)
+  const [hover, setHover] = useState(true)
+
+  const handelClick =(currentIdx)=>{
+    console.log(currentIdx);
+    setRating(currentIdx)
+    setHover(prev=>!prev)
+    
+  }
+  const handelMouseMove = (currentIdx)=>{
+    if(hover) setRating(currentIdx)
+  
+
+  }
+
+
+
+  return (
+    <div className="h-screen w-full bg-gray-300 flex justify-center items-center">
+      <div className="flex text-3xl ">
+        {arr.map((_,idx) => {
+          return <FaStar
+              className={idx <= rating ? "active" : "inactive"}
+              key = {idx+1}
+              onClick={()=>{handelClick(idx)}}
+              onMouseMove={()=>{handelMouseMove(idx)}}
+          />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default App;
