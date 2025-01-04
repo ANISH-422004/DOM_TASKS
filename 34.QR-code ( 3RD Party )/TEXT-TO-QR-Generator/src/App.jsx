23176fee6fe7cd33fa51879e7a98cca5badaf8a9
ANISH-422004 <anishbhattacharya422004@gmail.com>
2025-01-04 19:04:50 +0530

    React-Qr Done

---
+++ 34.QR-code ( 3RD Party )/TEXT-TO-QR-Generator/src/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useState } from 'react'
+import QRCode from 'react-qr-code'
+
+const App = () => {
+  const [Text , setText] = useState('')
+  const [toshow, setToshow] = useState('')
+
+  const HandelClick = ()=>{
+      
+      setToshow(Text)
+      setText('')
+  }
+
+
+  return (
+    <div className='h-screen w-full bg-cyan-900 flex flex-col gap-5 justify-center items-center'>
+
+      <div className='flex gap-3'> 
+        
+      <input
+      onChange={(e)=>{setText(e.target.value)}}
+      type="text" placeholder='Text To QR ...' />
+      <button 
+      className='border border-cyan-300 text-cyan-300 rounded '
+              onClick={
+                        HandelClick 
+              }
+      >
+        Generate Qr
+      </button>
+
+      </div>
+    <div className='w-[100%] flex justify-center items-center' >
+    <QRCode
+        style={{ height: "auto", maxWidth: "30%", width: "20%" }}
+        value={toshow}
+        />
+    </div>
+
+    </div>
+  )
+}
+
+export default App
